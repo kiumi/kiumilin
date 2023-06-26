41b0a94414f16a41ee65c56c4eac2ff9ad4c792f
Sophia <kiumiaiyu@gmail.com>
2023-06-26 22:01:43 +0800

    第一階段切版

--- src/pages/GalleryPage.vue
+++ src/pages/GalleryPage.vue
@@ -1,16 +1,42 @@
 <template>
   <div class="gallery">
-    <div>Gallery Page</div>
-    <nav >
+    <div class="web-company">
+      <div class="main-title">
+        <div class="title-en">Web Design</div>
+        <span></span>
+        <div class="title-tw">網頁設計</div>
+      </div>
+
+      <!-- 切換頁籤 -->
+      <div class="tab-list">
+        <div class="tab active">形象網站</div>
+        <div class="tab">遊戲娛樂</div>
+      </div>
+
+      <div class="web-content">
+        <a href="#" class="items"><img src="../../images/web_company/web_001.jpg" alt=""></a>
+        <a href="#" class="items"><img src="../../images/web_company/web_002.jpg" alt=""></a>
+        <a href="#" class="items"><img src="../../images/web_company/web_003.jpg" alt=""></a>
+        <a href="#" class="items"><img src="../../images/web_company/web_004.jpg" alt=""></a>
+        <a href="#" class="items"><img src="../../images/web_company/web_005.jpg" alt=""></a>
+        <a href="#" class="items"><img src="../../images/web_company/web_006.jpg" alt=""></a>
+
+      </div>
+
+      <a href="/" class="back-btn">
+        <span class="back-btn-icon"><img src="../../images/back-btn.svg" alt=""></span>
+          Back to Index
+      </a>
+    </div>
+
+    <!-- <nav >
       <div><RouterLink to="/">Index</RouterLink></div>
       <div><RouterLink to="/list">List</RouterLink></div>
       <div><RouterLink to="/gallery">Gallery</RouterLink></div>
-    </nav>
+    </nav> -->
   </div>
 </template>
 
 <style lang="scss">
-.gallery {
-  color: blue;
-}
+
 </style>
--- src/pages/ListPage.vue
+++ src/pages/ListPage.vue
@@ -1,16 +1,46 @@
 <template>
   <div class="list">
-    <div>List Page</div>
-    <nav >
+    <!-- 作品詳細 -->
+    <div class="project-detail">
+      <div class="main-title">
+        <div class="title-en">Project Details</div>
+        <span></span>
+        <div class="title-tw">娛樂平台</div>
+      </div>
+      <div class="detail-content">
+        <img src="../../images/project_001/project_001_1.webp" alt="">
+        <img src="../../images/project_001/project_001_2.webp" alt="">
+        <img src="../../images/project_001/project_001_3.webp" alt="">
+        <img src="../../images/project_001/project_001_4.webp" alt="">
+        <img src="../../images/project_001/project_001_5.webp" alt="">
+        <img src="../../images/project_001/project_001_6.webp" alt="">
+        <img src="../../images/project_001/project_001_7.webp" alt="">
+        <img src="../../images/project_001/project_001_8.webp" alt="">
+        <img src="../../images/project_001/project_001_9.webp" alt="">
+        <img src="../../images/project_001/project_001_10.webp" alt="">
+        <img src="../../images/project_001/project_001_11.webp" alt="">
+        <img src="../../images/project_001/project_001_12.webp" alt="">
+        <img src="../../images/project_001/project_001_13.webp" alt="">
+        <img src="../../images/project_001/project_001_14.webp" alt="">
+        <img src="../../images/project_001/project_001_15.webp" alt="">
+      </div>
+
+      <a href="/" class="back-btn">
+        <span class="back-btn-icon"><img src="../../images/back-btn.svg" alt=""></span>
+          Back to Index
+      </a>
+    </div>
+
+    <!-- <nav >
       <div><RouterLink to="/">Index</RouterLink></div>
       <div><RouterLink to="/list">List</RouterLink></div>
       <div><RouterLink to="/gallery">Gallery</RouterLink></div>
-    </nav>
+    </nav> -->
   </div>
 </template>
 
 <style lang="scss">
-.list {
-  color: green;
-}
+// .list {
+//   color: green;
+// }
 </style>
